2d1298c1fcd8aecc019be3b784d469f95fc85c44
Rob Duarte <me@robduarte.com>
2022-10-27 17:47:28 -0400

    Works

--- sketch.js
+++ sketch.js
@@ -1,7 +1,20 @@
+let img = [];
+
+function preload() {
+  for (let i = 0; i <= 8; i++) {
+    img[i] = loadImage("assets/" + i + ".png");
+  }
+}
+
 function setup() {
   createCanvas(400, 400);
+  slider = createSlider(0, 8, 0, 1);
+  slider.position(10,10);
+  slider.style('width','180px');
 }
 
 function draw() {
   background(125);
+
+  image(img[ slider.value() ], 10, 40, 128, 128);
 }
